925204cdc02cee5524fad15643eb0d5f5f8a2198
chenzhigang <979703986@qq.com>
2024-01-10 17:33:44 +0800

    chore: schema json options

--- packages/copy-loader/src/index.ts
+++ packages/copy-loader/src/index.ts
@@ -1,1 +1,9 @@
-import { LoaderContext } from 'webpack'
+import type { LoaderContext } from 'webpack'
+import type { CopyLoaderOptions } from './types'
+import schema from './schema.json'
+
+const s = schema
+
+export default function loader(this: LoaderContext<CopyLoaderOptions>, source: string) {
+  const { entryPath } = this.getOptions(s)
+}
