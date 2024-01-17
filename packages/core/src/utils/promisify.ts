e3ec1afca751cacd5e3b29b86b3284f24eaa18f5
chenzhigang <979703986@qq.com>
2024-01-17 13:18:51 +0800

    feat: promisify

--- packages/core/src/utils/promisify.ts
+++ packages/core/src/utils/promisify.ts
@@ -1,3 +1,31 @@
-export function promisify() {
-  //
+export type PromisifySuccessResult<
+  P,
+  T extends { success?: (...args: any[]) => void },
+> = P extends {
+  success: any
+}
+  ? void
+  : P extends { fail: any }
+    ? void
+    : P extends { complete: any }
+      ? void
+      : Promise<Parameters<Exclude<T['success'], undefined>>[0]>
+
+export type PromisifyRestArgs<T> = T extends [any, ...args: infer P] ? P : never
+
+export function promisify<T extends (...args: any[]) => any>(fn: T) {
+  return function <P extends Parameters<T>[0]>(
+    options: P,
+    ...args: PromisifyRestArgs<Parameters<T>>
+  ): PromisifySuccessResult<P, Parameters<T>[0]> {
+    const { success, fail, complete } = options as any
+
+    if (success || fail || complete) {
+      return fn(options, ...args)
+    }
+
+    return new Promise((resolve, reject) => {
+      return fn({ ...options, fail: reject, success: resolve }, ...args)
+    }) as any
+  }
 }
