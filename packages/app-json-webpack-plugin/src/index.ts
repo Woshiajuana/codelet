48afb64742ad3b4de8e42e25330eb104e4497321
chenzhigang <979703986@qq.com>
2024-01-17 22:55:53 +0800

    feat: app json webpack plugin

---
+++ packages/app-json-webpack-plugin/src/index.ts
@@ -0,0 +1,10 @@
+import path from 'path'
+import { Compilation, Compiler, sources, Chunk } from 'webpack'
+
+const NAME = 'AppJsonWebpackPlugin'
+
+export default class AppJsonWebpackPlugin {
+  apply(compiler: Compiler) {
+    //
+  }
+}
