e04c4c4e7e3f165c41f93a74235fd926762e19f5
chenzhigang <979703986@qq.com>
2024-02-23 11:25:43 +0800

    chore: 开始写 compiler

--- packages/wxml-loader/src/compiler/demo.ts
+++ packages/wxml-loader/src/compiler/demo.ts
@@ -1,3 +1,8 @@
+import { parse } from './parse.js'
 const content = `
   parse
 `
+
+const ast = parse(content)
+
+console.log('ast => ', ast)
--- packages/wxml-loader/src/compiler/parse.ts
+++ packages/wxml-loader/src/compiler/parse.ts
@@ -1,3 +1,3 @@
-export function parse() {
+export function parse(html: string) {
   //
 }
