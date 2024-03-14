c21a8a2e82e46afb3fa82bd9a5a079312dba1da2
chenzhigang <979703986@qq.com>
2024-03-14 22:34:57 +0800

    chore: webview

--- playground/src/pages/webview/index.ts
+++ playground/src/pages/webview/index.ts
@@ -2,12 +2,22 @@ import './index.json'
 import './index.wxml'
 import './index.scss'
 
-import { RouterBehavior, createPage } from '@codelet/core'
+import { formatPathParams } from '@daysnap/utils'
+import col, { RouterBehavior, createPage } from '@codelet/core'
 
 createPage({
   behaviors: [RouterBehavior],
   onLoad(query) {
     this.routerParseQuery(query)
+    this.initData()
+  },
+  initData() {
+    const { label, title, link } = this.data.query
+    col.setNavigationBarTitle({ title: title || label })
+    const { path: url } = formatPathParams(link, {
+      t: Date.now(), // 防止缓存
+    })
+    this.setData({ url })
   },
   handleMessage(event: any) {
     //
