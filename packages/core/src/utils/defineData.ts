1fda0a0dba93d035e0517f135ab89bbd47d44c58
chenzhigang <979703986@qq.com>
2024-03-17 22:08:27 +0800

    chore: 优化

--- packages/core/src/utils/defineData.ts
+++ packages/core/src/utils/defineData.ts
@@ -2,6 +2,10 @@ import { isFunction } from '@daysnap/utils'
 
 export type DefineData<T> = T | (() => T)
 
-export function defineData<T = Record<string, any>>(data: DefineData<T>): T {
+/**
+ * 小程序是选项式 api ，这个函数主要辅助定义 data 的类型
+ * 因此 data 可以定义任何类型 方便后续使用
+ */
+export function defineData<T = Record<string, any>>(data: DefineData<any> = {}): T {
   return isFunction(data) ? data() : data
 }
