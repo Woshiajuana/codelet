9b114cee28346e7c68a1b8f7ce3ab91eccf4066d
chenzhigang <979703986@qq.com>
2024-03-31 16:22:59 +0800

    chore: paging

--- packages/core/src/behaviors/paging.ts
+++ packages/core/src/behaviors/paging.ts
@@ -1,36 +1,80 @@
-import { createBehavior } from '../create'
+import type { Loose } from '@daysnap/types'
+import { isBoolean, isFunction, isObject } from '@daysnap/utils'
+
+import { createBehavior } from '../create/createBehavior'
+import { defineData, parseEvent } from '../utils'
 
 export const PagingBehavior = createBehavior({
-  data: {
+  data: defineData({
     pagingIndex: 1,
     pagingSize: 10,
     pagingTotal: -1,
-    pagingData: '', // 二维数组
+    pagingData: [], // 二维数组
     pagingIsLoading: false,
     pagingNumTotal: -1, // 加载的 total
-  },
+  }),
   methods: {
     /**
      * 刷新
      */
-    pagingRefresh(event?: unknown) {
-      // this.setData
-      // this.pa
+    pagingRefresh(event: any = {}) {
+      const { loading, callback, ...rest } = isObject(event)
+        ? parseEvent(event as any)
+        : {
+            loading: isBoolean(event) ? event : false,
+            callback: isFunction(event) ? event : null,
+          }
+      this.pagingTrigger(1, { ...rest, loading, callback })
     },
 
     /**
      * 请求数据
      */
-    async pagingTrigger(options: { pagingIndex: number }) {
+    async pagingTrigger(pagingIndex: number, options: PagingOptions = {}) {
       if (!this.pagingFetchData) {
         return console.error('pagingFetchData is not defined')
+      }
+
+      try {
+        // eslint-disable-next-line prefer-const
+        let { pagingSize, pagingData, pagingNumTotal } = this.data
+        const [list, pagingTotal] = await this.pagingFetchData([pagingIndex, pagingSize], options)
+        if (pagingIndex === 1) {
+          pagingData = []
+          pagingNumTotal = 0
+          pagingNumTotal = 0
+          pagingNumTotal += list.length
+          pagingData[0] = list
+          this.setData({
+            pagingNumTotal,
+            pagingTotal,
+            pagingData,
+            pagingIndex,
+          })
+        } else {
+          pagingNumTotal += list.length
+          this.setData({
+            pagingNumTotal,
+            pagingTotal,
+            pagingIndex,
+            [`pagingData[${pagingIndex - 1}]`]: list,
+          })
+        }
+        // eslint-disable-next-line no-useless-catch
+      } catch (error) {
+        throw error
+      } finally {
+        if (options.callback) {
+          options.callback()
+        }
+        this.setData({ pagingIsLoading: false })
       }
     },
 
     /**
      * 加载
      */
-    pagingLoad(options: any) {
+    pagingLoad(options?: any) {
       const { pagingTotal, pagingData, pagingNumTotal, pagingIsLoading, pagingIndex } = this.data
       if (pagingIsLoading) {
         return console.log('正在加载中...')
@@ -38,14 +82,24 @@ export const PagingBehavior = createBehavior({
       if (pagingData && pagingTotal <= pagingNumTotal) {
         return console.log('没有更多了...')
       }
-      this.pagingTrigger({ ...options, pagingIndex: pagingIndex + 1 })
+      this.pagingTrigger(pagingIndex + 1, options)
     },
 
     /**
      * 请求配置方法
      */
-    async pagingFetchData(pageIndex: number, loading: boolean) {
-      return ''
+    async pagingFetchData(_: PagingParams, __?: any): Promise<PagingResult<any>> {
+      // 请在页面中实现
+      return [] as any
     },
   },
 })
+
+export type PagingParams = [number, number]
+
+export type PagingResult<T = any> = [T[], number]
+
+export type PagingOptions = Loose<{
+  loading?: boolean
+  callback?: (...args: any) => any
+}>
