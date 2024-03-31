e84827202c876a226d74fc577526719ceac25a6d
chenzhigang <979703986@qq.com>
2024-03-31 09:07:01 +0800

    fix: 0.0.10

--- packages/core/src/utils/parseLocation.ts
+++ packages/core/src/utils/parseLocation.ts
@@ -14,7 +14,7 @@ export function parseLocation(
   }
   const { url, query = {}, ...rest } = location
   return {
-    url: `${url}?query=${JSON.stringify(query)}`,
+    url: `${url}?query=${encodeURIComponent(JSON.stringify(query))}`,
     ...rest,
   }
 }
