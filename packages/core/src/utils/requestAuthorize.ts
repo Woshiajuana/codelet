66d57c9ac7c8af1d59bffc3ed740ab36cf7de89c
chenzhigang <979703986@qq.com>
2024-04-01 13:16:17 +0800

    fix: request authorize

--- packages/core/src/utils/requestAuthorize.ts
+++ packages/core/src/utils/requestAuthorize.ts
@@ -3,22 +3,29 @@ import { col } from '../codelet'
 /**
  * 申请授权操作
  */
-export async function requestAuthorize(options: { scope: string; message?: string }) {
+export async function requestAuthorize(options: {
+  scope: keyof WechatMiniprogram.AuthSetting
+  message?: string
+}) {
   const { scope, message } = options
-  await col.authorize({
-    scope,
-  })
-  if (!message) {
-    throw new Error('进行此操作，需要授权')
-  }
 
-  await col.showModal({
-    title: '授权提示',
-    content: message,
-  })
-
-  const { authSetting } = await col.openSetting()
-  if (!(authSetting as any)[scope]) {
-    throw new Error('进行此操作，需要授权')
+  try {
+    await col.authorize({
+      scope,
+    })
+  } catch {
+    if (!message) {
+      throw '进行此操作，需要授权'
+    }
+    const res = await col.showModal({
+      title: '授权提示',
+      content: message,
+    })
+    if (res.confirm) {
+      const { authSetting } = await col.openSetting()
+      if (!authSetting[scope]) {
+        throw '进行此操作，需要授权'
+      }
+    }
   }
 }
