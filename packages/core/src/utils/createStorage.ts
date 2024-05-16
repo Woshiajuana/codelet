151cfca85a77cf7947ed984f1768da17d750926b
chenzhigang <979703986@qq.com>
2024-05-16 15:26:54 +0800

    chore: 新版本

--- packages/core/src/utils/createStorage.ts
+++ packages/core/src/utils/createStorage.ts
@@ -1,4 +1,8 @@
+import { isArray, isObject } from '@daysnap/utils'
+
 import { col } from '../codelet'
+
+type DefaultVal<T> = T extends Record<string, any> ? Partial<T> : T
 
 function setItem<T = any>(key: string, val: T) {
   col.setStorageSync<T>(key, val)
@@ -6,7 +10,7 @@ function setItem<T = any>(key: string, val: T) {
 }
 
 function getItem<T = any>(key: string): T | undefined
-function getItem<T = any>(key: string, defaultVal: Partial<T>): T
+function getItem<T = any>(key: string, defaultVal: DefaultVal<T>): T
 function getItem<T = any>(key: string, defaultVal?: any) {
   return col.getStorageSync<T>(key) || defaultVal
 }
@@ -15,9 +19,14 @@ function removeItem(key: string) {
   col.removeStorageSync(key)
 }
 
-function updateItem<T = any>(key: string, val: Partial<T>) {
-  const prev = getItem<T>(key, {})
-  return setItem<T>(key, { ...prev, ...val } as T)
+function updateItem<T = any>(key: string, val: DefaultVal<T>) {
+  const prev = getItem<T>(key)
+  if (isObject(val)) {
+    val = { ...(prev ?? {}), ...val }
+  } else if (isArray(val)) {
+    val = [...((prev as any[]) ?? []), ...val] as any
+  }
+  return setItem<T>(key, val as T)
 }
 
 export function createStorage<T = any>(key: string) {
@@ -32,7 +41,7 @@ export function createStorage<T = any>(key: string) {
 export interface StorageInstance<T = any> {
   setItem(val: T): T
   getItem(): T | undefined
-  getItem(defaultVal: Partial<T>): T
+  getItem(defaultVal: DefaultVal<T>): T
   removeItem(): void
-  updateItem(val: Partial<T>): T
+  updateItem(val: DefaultVal<T>): T
 }
