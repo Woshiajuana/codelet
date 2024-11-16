87b582f98a964b739c206b24d2a481a11406749f
chenzhigang <979703986@qq.com>
2024-11-16 23:22:20 +0800

    chore: core 0.0.27

--- packages/core/src/plugins/router.ts
+++ packages/core/src/plugins/router.ts
@@ -11,7 +11,7 @@ declare module '../codelet' {
     reLaunch(to: Location): Promise<void>
     // https://developers.weixin.qq.com/miniprogram/dev/api/route/wx.switchTab.html
     // 需要跳转的 tabBar 页面的路径 (代码包路径)（需在 app.json 的 tabBar 字段定义的页面），路径后不能带参数。
-    switchTab(to: string): Promise<void>
+    switchTab(to: Location): Promise<void>
   }
 }
 
