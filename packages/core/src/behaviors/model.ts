6c0f77818c3426197d2e2d59a31dff350e82241f
chenzhigang <979703986@qq.com>
2024-12-23 13:47:06 +0800

    feat: core modelTransfer pattern

--- packages/core/src/behaviors/model.ts
+++ packages/core/src/behaviors/model.ts
@@ -4,9 +4,13 @@ import { parseEvent } from '../utils'
 export const ModelBehavior = createBehavior({
   methods: {
     modelTransfer(e: any) {
-      const { prop, value, disabled } = parseEvent(e)
+      // eslint-disable-next-line prefer-const
+      let { prop, value, disabled, pattern, ...rest } = parseEvent(e)
       if (disabled) return
       if (prop) {
+        if (pattern && (this as any)[pattern]) {
+          value = (this as any)[pattern](value, { prop, ...rest })
+        }
         this.modelBeforeFieldChange(prop, value)
         this.setData({ [prop]: value })
         this.modelAfterFiledChange(prop, value)
