2b6e3e9a7775b33ef779a34588e97448c69497c6
chenzhigang <979703986@qq.com>
2024-12-24 10:03:50 +0800

    feat: core version 0.0.31

--- packages/core/src/behaviors/model.ts
+++ packages/core/src/behaviors/model.ts
@@ -1,16 +1,37 @@
+import { isArray, isFunction } from '@daysnap/utils'
+
 import { createBehavior } from '../create'
 import { parseEvent } from '../utils'
 
 export const ModelBehavior = createBehavior({
   methods: {
     modelTransfer(e: any) {
+      const { item, ...rest } = parseEvent(e)
       // eslint-disable-next-line prefer-const
-      let { prop, value, disabled, pattern, ...rest } = parseEvent(e)
+      let { prop, value, disabled, pattern, options, valueKey } = Object.assign({}, item, rest)
       if (disabled) return
-      if (prop) {
-        if (pattern && (this as any)[pattern]) {
-          value = (this as any)[pattern](value, { prop, ...rest })
+
+      if (isArray(options) && value >= 0) {
+        const option = options[value]
+        if (!option) {
+          return
         }
+        if (valueKey) {
+          value = option[valueKey]
+        } else {
+          value = option
+        }
+      }
+
+      if (pattern) {
+        if (isFunction(pattern)) {
+          value = pattern(value, item.value)
+        } else if ((this as any)[pattern]) {
+          value = (this as any)[pattern](value, item.value)
+        }
+      }
+
+      if (prop) {
         this.modelBeforeFieldChange(prop, value)
         this.setData({ [prop]: value })
         this.modelAfterFiledChange(prop, value)
