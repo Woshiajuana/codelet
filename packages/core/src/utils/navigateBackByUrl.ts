b9241e5a7510628b0a008d304ac96e87a0cba688
chenzhigang <979703986@qq.com>
2025-01-05 17:54:00 +0800

    feat: navigate back by url

--- packages/core/src/utils/navigateBackByUrl.ts
+++ packages/core/src/utils/navigateBackByUrl.ts
@@ -1,8 +1,8 @@
 import { col } from '../codelet'
 
-export function navigateBackByRoute(route: string) {
+export function navigateBackByUrl(url: string) {
   const pages = getCurrentPages()
-  const index = pages.findIndex((item) => item.route === route)
+  const index = pages.findIndex((item) => `/${item.route}` === url)
   let delta: number = 0
   if (index > -1) {
     delta = pages.length - index - 1
