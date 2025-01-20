dceec92a51daf309d521b08a0485a665a517ef68
chenzhigang <979703986@qq.com>
2025-01-20 17:40:39 +0800

    feat: core 发布

--- packages/core/src/utils/parseQuery.ts
+++ packages/core/src/utils/parseQuery.ts
@@ -10,7 +10,13 @@ export function parseQuery<T extends Record<string, any>>(options: Record<string
   // eslint-disable-next-line prefer-const
   let { query = {}, scene, ...rest } = options
   if (isString(query)) {
-    query = JSON.parse(decodeURIComponent(query))
+    query = decodeURIComponent(query)
+    if (query.startsWith('%7B')) {
+      // 为了兼容开发者工具的编译模式，路径参数会被 encodeURIComponent
+      // 这里再一次解码下
+      query = decodeURIComponent(query)
+    }
+    query = JSON.parse(query)
   }
   if (scene) {
     if (scene.includes('=')) {
