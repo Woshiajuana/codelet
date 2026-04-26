e4f208805597741bbd493197a4355d7e373dbc6b
chenzhigang <979703986@qq.com>
2026-04-26 11:02:19 +0800

    chore: 支持输出的产物是 es6 语法

--- playground/babel.config.js
+++ playground/babel.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   // https://babel.nodejs.cn/docs/options/#sourcetype
   sourceType: 'unambiguous',
-  presets: ['@babel/preset-typescript', '@babel/preset-env'],
+  presets: [
+    '@babel/preset-typescript',
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+  ],
   plugins: ['@babel/plugin-transform-runtime'],
 }
